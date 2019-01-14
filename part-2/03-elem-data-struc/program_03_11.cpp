d03995c3898483767dac01e10013b8294d7ea73e
Juri <23640912+jthommen@users.noreply.github.com>
2019-01-14 13:15:30 +0100

    Chapter 3 update

--- part-2/03-elem-data-struc/program_03_11.cpp
+++ part-2/03-elem-data-struc/program_03_11.cpp
@@ -36,20 +36,36 @@ typedef node* link;
 int main(int argc, char *argv[])
 {
     // Command line arguments check
-    if(argc<1) 
+    if(argc<2) 
     {
         std::cerr << "Need one command line argument.\n";
         return -1;
     }
     int i,  N = atoi(argv[1]);
 
-    node heada(0,0); link a = &heada, t=a;
+    // Create 'dummy' head node called head a
+    node heada(0,0);
+    link t, a = &heada;
+    t=a;
 
+    // Making linked list (input, unsorted)
     for(i=0; i<N; i++)
         t = (t-> next = new node(rand() % 1000, 0));
     
-    node headb(0,0); link u, x, b = &headb;
-    for(x = b; x -> next != 0; x = x->next)
-        if(x->next->item > t->item) break;
-    t->next = x->next; x->next = t;
+    // Making second head node
+    node headb(0,0);
+    link u, x, b = &headb;
+
+    // Main loop: Iterate through list a and insert in list b in order
+    for(t = a->next; t != 0; t = u) // go over all elements in a
+    {
+        u = t->next;
+        // find item in b which is just bigger than item from a
+        for(x = b; x->next != 0; x = x->next)
+            if(x->next->item > t->item) break;
+        // insert item from a in b 
+        t->next = x->next;
+        // tackle next item
+        x->next = t;
+    }
 }
